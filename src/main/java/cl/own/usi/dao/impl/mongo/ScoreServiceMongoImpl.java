91b8958f5db36ed160b521f0b3f3e8d2cd8c3cea
Benoit Perroud <benoit@noisette.ch>
2011-02-20 09:51:42 +0100

    Add missing files for mongo DAO.

---
+++ src/main/java/cl/own/usi/dao/impl/mongo/ScoreServiceMongoImpl.java
@@ -0,0 +1,10 @@
+package cl.own.usi.dao.impl.mongo;
+
+import org.springframework.stereotype.Repository;
+
+import cl.own.usi.dao.impl.memory.ScoreDAOImpl;
+
+@Repository
+public class ScoreServiceMongoImpl extends ScoreDAOImpl {
+
+}
---
+++ src/main/java/cl/own/usi/dao/impl/mongo/UserDAOMongoImpl.java
@@ -0,0 +1,10 @@
+package cl.own.usi.dao.impl.mongo;
+
+import org.springframework.stereotype.Repository;
+
+import cl.own.usi.dao.impl.memory.UserDAOImpl;
+
+@Repository
+public class UserDAOMongoImpl extends UserDAOImpl {
+
+}
