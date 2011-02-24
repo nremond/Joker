9a52a0eb6b1f11cf7167bbaf115dca080da0ad5a
Reynald Borer <reynald.borer@gmail.com>
2011-02-24 22:32:16 +0100

    Enable correct logging through LogBack
    Add standard logback.xml files for logging of the frontend & backend. Log files will be automatically rotated every day.

--- frontend/src/main/java/cl/own/usi/main/GatewayMain.java
+++ frontend/src/main/java/cl/own/usi/main/GatewayMain.java
@@ -1,13 +1,27 @@
 package cl.own.usi.main;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class GatewayMain {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(GatewayMain.class);
+
 	public static void main(String[] args) {
 
-		String springContextFile = "classpath*:gatewayApplication.xml";
-		new ClassPathXmlApplicationContext(springContextFile);
+		final String springContextFile = "classpath*:gatewayApplication.xml";
 
+		LOGGER.info("Starting up Gateway...");
+
+		final ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				springContextFile);
+
+		final Date startupDate = new Date(applicationContext.getStartupDate());
+		LOGGER.info("Started at {}", startupDate);
 	}
 }
